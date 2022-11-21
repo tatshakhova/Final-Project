6d7ec25c2fc3951f8b11293a212ab80a89a4ead7
Tati <tatshakhova@gmail.com>
2022-11-22 00:25:05 +0300

    Create function Count

--- Final/Program.cs
+++ Final/Program.cs
@@ -1,1 +1,13 @@
 ﻿string[] Array = new string[] {"Hello", "2", "world", ":-D"};
+void Count(string[] array)
+{ 
+    Console.WriteLine("Массив из строк, длина которых меньше либо равна 3 символам:");
+    foreach(string el in array)
+    {
+        if (el.Length <= 3) 
+        {
+           Console.Write($"{el} ");
+        }
+    }
+
+}
